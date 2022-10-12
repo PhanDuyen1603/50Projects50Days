7ab907c3807fb65ab735446358de456f3497adf5
antcb <antcb@viettel.com.vn>
2022-10-12 22:25:56 +0700

    change navbar to open new window. exercise 4 click outside to close search

--- 04/script.js
+++ 04/script.js
@@ -6,3 +6,22 @@ btn.addEventListener('click',() =>{
   search.classList.toggle('active');
   input.focus();
 })
+
+
+/**
+ * CLOSE SEARCH BAR WHEN CLICK OUTSIDE 
+ * IDEA ADD CLICK EVENT TO DOCUMENT (WHOLE PAGE)
+ * CHECK CLASSLIST OF CLICK EVENT
+ * IF NULL, THEN THE CLICK IS OUTSIDE ELEMENT(MAYBE BETTER LOGIC HERE)
+ * CHECK SEARCH BAR IS ACTIVE OR NOT
+ * IF SO, TOGGLE ACTIVE CLASS
+ */
+document.addEventListener('click', (e) => {
+  console.log(e.target.classList.value);
+  console.log(search.classList.value);
+  if(!e.target.classList.value){
+    if(search.classList.value === 'search active'){
+      search.classList.toggle('active');
+    }
+  }
+})
