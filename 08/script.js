ec4507c1d621842dcceece5d2a917ab90aa8353c
duyenphan <duyenptm8@viettel.com.vn>
2022-10-17 22:23:07 +0700

    08 - Form Wave

--- 08/script.js
+++ 08/script.js
@@ -1,9 +1,16 @@
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const container = document.querySelector('.container')
+// const left = document.querySelector('.left')
+// const right = document.querySelector('.right')
+// const container = document.querySelector('.container')
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+// left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+// left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
-right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+// right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+// right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText.split(" ").map((letter, idx) =>`<span
+    style="transition-delay:${idx*50}ms">${letter}</span>`)
+});
