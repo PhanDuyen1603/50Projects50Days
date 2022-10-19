522677e309328283d24dd397f17264e11c3418dd
duyenphan <duyenptm8@viettel.com.vn>
2022-10-19 13:32:38 +0700

    10 - Dad Jokes

--- 10/script.js
+++ 10/script.js
@@ -1,26 +1,45 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+// const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
-sounds.forEach(sound => {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
+// sounds.forEach(sound => {
+//     const btn = document.createElement('button');
+//     btn.classList.add('btn');
 
-    btn.innerText= sound;
-    btn.addEventListener('click', ()=>{
-        stopSound();
-        document.getElementById(sound).play();
-    })
+//     btn.innerText= sound;
+//     btn.addEventListener('click', ()=>{
+//         stopSound();
+//         document.getElementById(sound).play();
+//     })
 
-    document.getElementById('buttons').appendChild(btn);
+//     document.getElementById('buttons').appendChild(btn);
 
 
-});
+// });
 
-function stopSound(){
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound);
-        song.pause();
-        song.currentTime = 0;
-    });
-    
+// function stopSound(){
+//     sounds.forEach(sound => {
+//         const song = document.getElementById(sound);
+//         song.pause();
+//         song.currentTime = 0;
+//     });
+
+// }
+
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke)
+    generateJoke();
+
+async function generateJoke(){
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config);
+    const data = await res.json();
+    jokeEl.innerHTML = data.joke;
 }
+
 
