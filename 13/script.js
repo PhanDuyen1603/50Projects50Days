d557f5a73f20be7d998358dc220e3bc1b76db88a
duyenphan <duyenptm8@viettel.com.vn>
2022-10-25 22:20:23 +0700

    13 - Random Choice Picker

--- 13/script.js
+++ 13/script.js
@@ -1,18 +1,4 @@
-const insert = document.getElementById('insert')
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
 
-window.addEventListener('keydown', (event) => {
-  insert.innerHTML = `
-  <div class="key">
-  ${event.key === ' ' ? 'Space' : event.key} 
-  <small>event.key</small>
-</div>
-<div class="key">
-  ${event.keyCode}
-  <small>event.keyCode</small>
-</div>
-<div class="key">
-  ${event.code}
-  <small>event.code</small>
-</div>
-  `
-})
+textarea.focus();
