cda1de3a959e0fefc9ff050288b3e7cd05f5bd6a
DuyenPhan <20120567@student.hcmus.edu.vn>
2022-10-26 09:28:41 +0700

    14 - Animated Nav

--- 13/script.js
+++ 13/script.js
@@ -2,3 +2,11 @@ const tagsEl = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
 
 textarea.focus();
+
+textarea.addEventListener('keyup', (e)=>{
+  
+})
+
+function createTags(input){
+  const tags = input.split(',').filters
+}
